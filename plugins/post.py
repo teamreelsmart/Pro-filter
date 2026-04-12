09e15bbfa06a4d590f17f84f962f01da6f721b23
teamreelsmart <team.reelsmart@gmail.com>
2026-04-12 07:12:09 +0530

    Update post.py

--- plugins/post.py
+++ plugins/post.py
@@ -17,8 +17,8 @@ logger = logging.getLogger(__name__)
 post_sessions = {}
 
 USE_DEFAULT_BTN = True
-DEFAULT_BTN_LINK = "https://telegram.me/Techifybots"
-DEFAULT_WATERMARK = "<blockquote><b><i>ʙʏ - @TechifyBots</i></b></blockquote>"
+DEFAULT_BTN_LINK = "https://telegram.me/TheOrviX"
+DEFAULT_WATERMARK = "<blockquote><b><i>ʙʏ - @OrvixMovies</i></b></blockquote>"
 LANGUAGES_FORMAT = "<b>‣ ᴀᴜᴅɪᴏ :</b> {langs}"
 SUBTITLES_FORMAT = "<b>‣ sᴜʙᴛɪᴛʟᴇ :</b> {subs}"
 RESOLUTIONS_FORMAT = "<b>‣ ǫᴜᴀʟɪᴛʏ :</b> {resolutions}"
